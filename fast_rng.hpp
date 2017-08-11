2cd6c5392bb8c9ecbbf91eecd53579b45182e943
mburhanpurkar <maya.burhanpurkar@gmail.com>
2017-08-11 10:01:40 -0400

    Split generation into a function to generate 256 random bits and another to interpret as 8 signed floats

--- fast_rng.hpp
+++ fast_rng.hpp
@@ -67,9 +67,8 @@ struct vec_xorshift_plus
       s1 = _s1;
     };
 
-    // Generates 256 random bits (interpreted as 8 signed floats)
-    // Returns an __m256 vector, so bits must be stored using _mm256_storeu_ps() intrinsic!
-    inline __m256 gen_floats()
+    // Generates 256 random bits
+    inline __m256i gen_rand_bits()
     {
         // x = s0
         __m256i x = s0;
@@ -83,11 +82,18 @@ struct vec_xorshift_plus
         s1 = _mm256_xor_si256(x, y);
         s1 = _mm256_xor_si256(s1, _mm256_srli_epi64(x, 17));
         s1 = _mm256_xor_si256(s1, _mm256_srli_epi64(y, 26));
-      
+
+	return _mm256_add_epi64(y, s1);
+    }
+
+    // Interpret as 8 flots
+    inline __m256 gen_floats()
+    {
         // Convert to 8 signed 32-bit floats in range (-1, 1), since we multiply by 
         // a prefactor of 2^(-31)
-        return _mm256_mul_ps(_mm256_cvtepi32_ps(_mm256_add_epi64(y, s1)), _mm256_set1_ps(4.6566129e-10));
-    }
+        return _mm256_mul_ps(_mm256_cvtepi32_ps(gen_rand_bits()), _mm256_set1_ps(4.6566129e-10));
+    }  
+
 };
 
 } // namespace fast_rng
